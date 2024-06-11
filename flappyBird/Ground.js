15554f152204dbc8460e4ae36cfc2e714b56029f
Bluestimes <nikom-d@t-online.de>
2024-06-11 20:02:54 +0200

    Refactoring

--- flappyBird/Ground.js
+++ flappyBird/Ground.js
@@ -1,32 +1,31 @@
 class Ground {
 
-  constructor() {
-    this.height = 30;
-    this.topPixelCoord = canvas.height - this.height;
-    this.pixelOffset = 0;
+    constructor () {
+        this.height = 30;
+        this.topPixelCoord = canvas.height - this.height;
+        this.pixelOffset = 0;
 
-  }
-
-  show() {
-    fill(0);
-    rect(0, this.topPixelCoord, canvas.width, this.height);
-    for (var i = this.pixelOffset; i < canvas.width; i += groundSprite.width) {
-      image(groundSprite, i, this.topPixelCoord);
     }
-  }
 
-
-
-  update() {
-    this.pixelOffset -= panSpeed;
-    if (this.pixelOffset <= -groundSprite.width) {
-      this.pixelOffset += groundSprite.width;
+    show () {
+        fill(0);
+        rect(0, this.topPixelCoord, canvas.width, this.height);
+        for (let i = this.pixelOffset; i < canvas.width; i += groundSprite.width) {
+            image(groundSprite, i, this.topPixelCoord);
+        }
     }
 
 
-  }
+    update () {
+        this.pixelOffset -= panSpeed;
+        if (this.pixelOffset <= -groundSprite.width) {
+            this.pixelOffset += groundSprite.width;
+        }
 
-  collided(p) {
-    return p.y + p.size / 2 >= this.topPixelCoord;
-  }
+
+    }
+
+    collided (p) {
+        return p.y + p.size / 2 >= this.topPixelCoord;
+    }
 }
--- flappyBird/Node.js
+++ flappyBird/Node.js
@@ -1,71 +1,63 @@
 class Node {
+    constructor (number) {
+        this.number = number;
+        this.weightedSum = 0; // current sum i.e. before activation
+        this.activatedWeightedSum = 0; // after activation function is applied
+        this.outputConnections = []; // new ArrayList<ConnectionGene>();
+        this.layer = 0;
+        this.drawPos = createVector();
+    }
 
-  constructor(no) {
-    this.number = no;
-    this.inputSum = 0; //current sum i.e. before activation
-    this.outputValue = 0; //after activation function is applied
-    this.outputConnections = []; //new ArrayList<connectionGene>();
-    this.layer = 0;
-    this.drawPos = createVector();
-  }
-
-  //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-  //the node sends its output to the inputs of the nodes its connected to
-  engage() {
-      if(this.layer != 0) { //no sigmoid for the inputs and bias
-        this.outputValue = this.sigmoid(this.inputSum);
-      }
-
-      for(var i = 0; i < this.outputConnections.length; i++) { //for each connection
-        if(this.outputConnections[i].enabled) { //dont do shit if not enabled
-          this.outputConnections[i].toNode.inputSum += this.outputConnections[i].weight * this.outputValue; //add the weighted output to the sum of the inputs of whatever node this node is connected to
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // the node sends its output to the inputs of the nodes its connected to
+    engage () {
+        if (this.layer != 0) { // no sigmoid for the inputs and bias
+            this.activatedWeightedSum = Node.sigmoid(this.weightedSum);
         }
-      }
+
+        for (let i = 0; i < this.outputConnections.length; i++) { // for each connection
+            if (!this.outputConnections[i].enabled) continue;
+            
+            this.outputConnections[i].toNode.inputSum += this.outputConnections[i].weight * this.activatedWeightedSum; // add the weighted output to the sum of the inputs of whatever node this node is connected to
+        }
     }
-    //----------------------------------------------------------------------------------------------------------------------------------------
-    //not used
-   stepFunction(x) {
-      if(x < 0) {
-        return 0;
-      } else {
-        return 1;
-      }
+    
+    static sigmoid (x) {
+        return 1.0 / (1.0 + pow(Math.E, -4.9 * x));
     }
-    //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    //sigmoid activation function
-  sigmoid(x) {
-      return 1.0 / (1.0 + pow(Math.E, -4.9 * x)); //todo check pow
+
+    static stepFunction (x) {
+        return Math.max(x, 0);
     }
-    //----------------------------------------------------------------------------------------------------------------------------------------------------------
-    //returns whether this node connected to the parameter node
-    //used when adding a new connection
-  isConnectedTo(node) {
-      if(node.layer == this.layer) { //nodes in the same this.layer cannot be connected
+    
+    // ----------------------------------------------------------------------------------------------------------------------------------------------------------
+    // returns whether this node connected to the parameter node
+    // used when adding a new connection
+    isConnectedTo (node) {
+        if (node.layer == this.layer) { // nodes in the same this.layer cannot be connected
+            return false;
+        }
+
+        if (node.layer < this.layer) {
+            checkConnectionsFor(node, this);
+        } else {
+            checkConnectionsFor(this, node);
+        }
+
         return false;
-      }
-
-      //you get it
-      if(node.layer < this.layer) {
-        for(var i = 0; i < node.outputConnections.length; i++) {
-          if(node.outputConnections[i].toNode == this) {
-            return true;
-          }
+        
+        function checkConnectionsFor(origin, target){
+            for (let connection of origin.outputConnections){
+                if(connection.toNode == target) return true;
+            }
         }
-      } else {
-        for(var i = 0; i < this.outputConnections.length; i++) {
-          if(this.outputConnections[i].toNode == node) {
-            return true;
-          }
-        }
-      }
-
-      return false;
     }
-    //---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    //returns a copy of this node
-  clone() {
-    var clone = new Node(this.number);
-    clone.layer = this.layer;
-    return clone;
-  }
+
+    // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    // returns a copy of this node
+    clone () {
+        let clone = new Node(this.number);
+        clone.layer = this.layer;
+        return clone;
+    }
 }
